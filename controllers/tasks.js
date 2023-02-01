d50e2ffb0c2249f57d4caa4611e906c35303d8c3
Joshua Morgan <jmorg0605@gmail.com>
2023-02-01 15:18:57 +0800

    setup routes and controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,28 @@
 const getAllTasks = (req, res, next) => {
-  res.send('all items from the file');
+  res.send('get all tasks ');
 }
+
+const createTask = (req, res, next) => {
+  res.send('create new task');
+}
+
+const getTask = (req, res, next) => {
+  res.send('get single task');
+}
+
+const updateTask = (req, res, next) => {
+  res.send('update task');
+}
+
+const deleteTask = (req, res, next) => {
+  res.send('delete task');
+}
+
 
 module.exports = {
   getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask
 }
