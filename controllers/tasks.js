1ca424f57ee3c8b83ae37adbb6aaddec815abea2
Joshua Morgan <jmorg0605@gmail.com>
2023-02-01 15:36:17 +0800

    tested simple routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,28 +1,27 @@
 const getAllTasks = (req, res, next) => {
-  res.send('get all tasks ');
-}
+    res.send('get all tasks ');
+};
 
 const createTask = (req, res, next) => {
-  res.send('create new task');
-}
+    res.json(req.body);
+};
 
 const getTask = (req, res, next) => {
-  res.send('get single task');
-}
+    res.json({ id: req.params.id });
+};
 
 const updateTask = (req, res, next) => {
-  res.send('update task');
-}
+    res.send('update task');
+};
 
 const deleteTask = (req, res, next) => {
-  res.send('delete task');
-}
-
+    res.send('delete task');
+};
 
 module.exports = {
-  getAllTasks,
-  createTask,
-  getTask,
-  updateTask,
-  deleteTask
-}
+    getAllTasks,
+    createTask,
+    getTask,
+    updateTask,
+    deleteTask,
+};
