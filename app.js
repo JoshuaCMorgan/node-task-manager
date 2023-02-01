08bd9ecea0eaf19351d0d5e734c3631ed05c47e8
Joshua Morgan <jmorg0605@gmail.com>
2023-02-01 09:08:57 +0800

    setup basic routes and controlloers

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const tasksRoutes = require('./routes/tasks')
+
+// middleware
+app.use(express.json());
 
 // routes
 app.get('/hello', (req, res, next) => {
     res.send('Task Manager App');
 });
+
+app.use('/api/v1/tasks', tasksRoutes);
+
 
 app.listen(port, console.log(`Server is listening on port ${port}...`));
