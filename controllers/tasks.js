c14689eae2a44a68a7ba38a0ebe8135b5728eb2a
Joshua Morgan <jmorg0605@gmail.com>
2023-02-02 09:12:21 +0800

    setup remaining controllers

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -31,12 +31,31 @@ const getTask = async (req, res, next) => {
   }
 };
 
-const updateTask = (req, res, next) => {
-  res.send('update task');
+const updateTask = async (req, res, next) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, { new: true, runValidators: true });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
-const deleteTask = (req, res, next) => {
-  res.send('delete task');
+const deleteTask = async (req, res, next) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = {
