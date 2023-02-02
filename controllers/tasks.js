c8a97ff9465d0a95234566e8fe079b8947d8e119
Joshua Morgan <jmorg0605@gmail.com>
2023-02-02 08:28:53 +0800

    added error handling for createTask route

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,30 +1,34 @@
 const Task = require('../models/Task');
 
 const getAllTasks = (req, res, next) => {
-    res.send('get all tasks ');
+  res.send('get all tasks ');
 };
 
 const createTask = async (req, res, next) => {
+  try {
     const task = await Task.create(req.body);
     res.status(201).json({ task });
+  } catch(error) {
+    res.status(500).json({ msg: error })
+  }
 };
 
 const getTask = (req, res, next) => {
-    res.json({ id: req.params.id });
+  res.json({ id: req.params.id });
 };
 
 const updateTask = (req, res, next) => {
-    res.send('update task');
+  res.send('update task');
 };
 
 const deleteTask = (req, res, next) => {
-    res.send('delete task');
+  res.send('delete task');
 };
 
 module.exports = {
-    getAllTasks,
-    createTask,
-    getTask,
-    updateTask,
-    deleteTask,
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 };
